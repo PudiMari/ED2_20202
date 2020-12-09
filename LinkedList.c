58ba7b8065ba89b97aecbbd6b3d7f5ed6a283af7
nairamouras <nairamouras@gmail.com>
2020-12-09 18:16:42 -0400

    fix: Correção e tópico 3.2.13 adicionado

--- LinkedList.c
+++ LinkedList.c
@@ -6,16 +6,16 @@
   *     3.2.4 first --> Vinícius
   *     3.2.5 last --> Vinícius
   *     3.2.6 dequeue --> Mariana
-  *     3.2.7 pop 
-  *     3.2.8 top 
+  *     3.2.7 pop --> Kamilla
+  *     3.2.8 top --> Kamilla
   *     3.2.9 push --> Mariana
   *     3.2.10 getNodeByPos --> Katia
   *     3.2.11 getPos --> Katia
-  *     3.2.12 add
-  *     3.2.13 addAll
+  *     3.2.12 add --> Gustavo
+  *     3.2.13 addAll --> Naíra
   *     3.2.14 removePos --> Leonardo
   *     3.2.15 indexOf --> Leonardo
-  *     3.2.16 removeData
+  *     3.2.16 removeData --> Gustavo
   *4 LinkedListTest.c --> Kamilla
   */
  #include <stdio.h>
@@ -28,12 +28,12 @@ void init(LinkedList *list) {
     list->size=0;
 }
 
-// isEmpty by naíra
+//Verifica se a lista está vazia by Naíra
 bool isEmpty(LinkedList *list){
     return(list->size==0);
 }
 
-// enqueue by naíra
+//Insere um elemento ao final da lista by Naíra
 int enqueue(LinkedList *list, void *data){
     Node *newNode = (Node*)malloc(sizeof(Node));
     if(newNode==NULL) return -1;
@@ -66,8 +66,7 @@ void* last(LinkedList *list) {
     return data;
 }
 
-}
-//para usar a pilha, reaproveita-se as opreações da fila
+//para usar a pilha, reaproveita-se as operações da fila
 
 //pop
 void* pop(LinkedList *list) {
@@ -77,6 +76,25 @@ void* pop(LinkedList *list) {
 //top
 void* top(LinkedList *list) {
     return first(list);
+}
+
+//Adiciona uma lista dentro de outra lista a partir de uma determinada posição by Naíra
+int AddAll(LinkedList *listDest, int pos, LinkedList *listSource){
+    if(listDest==NULL || isEmpty(listDest)) return -1;
+    if(listSource==NULL || isEmpty(listSource)) return -2;
+    Node *last = NULL;
+    for(last = listSource->first; last->next!=NULL; last = last->next);
+    if(pos==0){
+        last->next = listDest->first;
+        listDest->first = listSource->first;
+    }else{
+        Node *aux = getNodeByPos(listDest, (pos-1));
+        if(aux==NULL) return -3;
+        last->next = aux->next;
+        aux->next = listSource->first;
+    }
+    listDest->size+=listSource->size;
+    return listSource->size;
 }
 
 // remove um elemento da lista de uma posicao fornecida. by Leonardo
