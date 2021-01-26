ac5db399a3909943739734f9ef401f715c85567b
kosugue <kamillamsrosa@gmail.com>
2021-01-26 16:44:48 -0400

    arquivo principal

---
+++ Hash.c
@@ -0,0 +1,11 @@
+/*5.1 Hash.h --> Kamilla
+ *   5.2 Hash.c --> Kamilla
+ *       5.2.1 initHash --> Vinícius 
+ *       5.2.2 hash --> Vinícius 
+ *       5.2.3 containsKey - Gustavo
+ *       5.2.4 put - Naíra
+ *       5.2.5 get - Katia  
+ *       5.2.6 removeKey - Mariana
+ *       5.2.7 showHashStruct - Leonardo
+*6 HashTest.c
+*/
