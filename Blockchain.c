ecbd2b702e6e3bea56da295a1f57d16f8ddb95c9
nairamouras <nairamouras@gmail.com>
2021-02-03 16:48:47 -0400

    Add initBlockchain e isValidNewBlock by Naíra

--- Blockchain.c
+++ Blockchain.c
@@ -1,7 +1,9 @@
 /* Blockchain.h --> Kamilla
  * BlockchainTest.c --> Kamilla
  * sha-256.c --> Kamilla
- * Blockchain.c -->     Anotem os blocos que fizeram com seus nomes :)
+ * Blockchain.c:
+ * InitBlockchain --> Naíra
+ * IsValidNewBlock --> Naíra
  */
 
 
@@ -9,3 +11,32 @@
 #include <string.h>
 #include "sha-256.c"
 #include <time.h>
+
+
+//Inicialização da estrutura by Naíra
+void initBlockchain(Blockchain *blockchain){
+    Block *genesisBlock = (Block*)malloc(sizeof(Block));
+    genesisBlock->index = 0;
+    genesisBlock->previousHash = "0";
+    genesisBlock->previousBlock = NULL;
+    genesisBlock->timestamp = time(NULL);
+    genesisBlock->data = 1000000;
+
+    genesisBlock->hash = calculateHash(genesisBlock->index, genesisBlock->previousHash,
+genesisBlock->timestamp, genesisBlock->data);
+
+    blockchain->genesisBlock = genesisBlock;
+    blockchain->latestBlock = genesisBlock;
+}
+
+//Valida a integridade de um bloco by Naíra
+bool isValidNewBlock(Block* newBlock, Block* previousBlock){
+    if(previousBlock->index+1 != newBlock->index){
+        return false;
+    }else if(previousBlock->hash != newBlock->previousHash){
+        return false;
+    }else if(strcmp(calculateBlockHash(newBlock), newBlock->hash) != 0){
+        return false;
+    }
+    return true;
+}
